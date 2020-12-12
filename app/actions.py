0024141e2d8af2dd614b10a528e627f8a3407fca
carolstoffel <carol_stoffel@hotmail.com>
2020-12-12 16:01:21 -0300

    melhorias no código

--- app/actions.py
+++ app/actions.py
@@ -7,9 +7,8 @@ import requests
 import geocoder
 import json
 
-weather_dict = {'freezing_rain_heavy': 'Chuva forte e neve.', 'freezing_rain': 'Chuva e neve lá fora.', 'freezing_rain_light': 'Chuva leve e neve.', 'freezing_drizzle': 'Garoa e neve.', 'ice_pellets_heavy': 'Muito granizo.', 'ice_pellets': 'Está chovendo gelo.', 'ice_pellets_light': 'Um pouco de granizo caindo.', 'snow_heavy': 'Chuva forte.', 'snow': 'Está nevando.', 'snow_light': 'Um pouco de neve.', 'flurries': 'Rajadas de vento.',
-                'tstorm': 'Tempestade por perto.', 'rain_heavy': 'Chuva forte.', 'rain': 'Está chovendo.', 'rain_light': 'Chuva leve.', 'drizzle': 'Garoa.', 'fog_light': 'Neblina.', 'fog': 'Névoa.', 'cloudy': 'Nublado.', 'mostly_cloudy': 'Muitas nuvens lá fora.', 'partly_cloudy': 'Parcialmente nublado.', 'mostly_clear': 'Ensolarado com poucas nuvens.', 'clear': 'O céu está limpo e o dia está ensolarado.'}
-url = "https://api.climacell.co/v3/weather/realtime"
+openweathermap_KEY = 'b4ac2ea46e84c8bb81af77cf3549ffb5'
+tomtom_KEY = 'thfGJoM8E7CjMZfbvf0Jw8i2Zbl3lPSF'
 
 
 class ActionAskWeather(Action):
@@ -17,7 +16,6 @@ class ActionAskWeather(Action):
         return "action_ask_weather"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        # print(address, (latitude, longitude)=geolocator.geocode("175 5th Avenue NYC"))
         result = ""
         local = next(tracker.get_latest_entity_values('GPE'), None)
         if local is None:
@@ -25,22 +23,22 @@ class ActionAskWeather(Action):
             result = "Não entendi o local que deseja. Vou dizer a temperatura de Toledo, Paraná, Brazil. "
         else:
             geolocation = geocoder.tomtom(
-                location=local, key='DIGITE SUA KEY DO TOMTOM AQUI')
+                location=local, key=tomtom_KEY)
             geo_json = geolocation.json
             geo_json = json.dumps(geo_json)
             geo_json = json.loads(geo_json)
             latitude, longitude = str(geo_json['lat']), str(geo_json['lng'])
 
-        querystring = {"lat": latitude, "lon": longitude, "unit_system": "si",
-                       "fields": "temp,humidity,weather_code", "apikey": "DIGITE SUA KEY DO CLIMACELL AQUI"}
-        response = requests.request("GET", url, params=querystring)
+        url = "http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}&units=metric&lang=pt_br".format(
+            latitude, longitude, openweathermap_KEY)
 
+        response = requests.request("GET", url)
         json_data = response.json()
-
-        if(json_data['weather_code']['value'] in weather_dict):
-            result += weather_dict[json_data['weather_code']['value']] + ' '
-        result += 'A temperatura é de %s%s, umidade de %s%s.' % (
-            json_data['temp']['value'], json_data['temp']['units'], json_data['humidity']['value'], json_data['humidity']['units'])
+        description = json_data['weather'][0]['description'].capitalize()
+        temperatura = json_data['main']['temp']
+        umidade = json_data['main']['humidity']
+        result += '{}. A temperatura no momento é de {}°C e umidade do ar de {}%.'.format(
+            description, temperatura, umidade)
 
         dispatcher.utter_message(text=result)
 
